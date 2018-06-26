9f266ad924f0707658b476a6fbeb57dbab1dbb2e
Guilherme Lima <guilherme@amoestudar.onmicrosoft.com>
2018-06-26 00:01:49 -0300

    Novas alterações.

--- DONE/Program.cs
+++ DONE/Program.cs
@@ -1,34 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Runtime.InteropServices;
-using Microsoft.VisualBasic;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace SimpleChromepwrec
 {
     /*
-     <~> Simple module that shows locally stored passwords and cookies in Google Chrome
-     <~> No external DDLs or other dependencies needed. SQLiteHandler and other classes are already here :--)
-    
-         */
+     <~> Módulo simples que mostra senhas e cookies armazenados localmente no Google Chrome.
+     <~> Não há DDLs externos ou outras dependências necessárias. SQLiteHandler e outras classes já estão aqui.
+    */
     class Program
     {
         static void Main(string[] args)
         {
-            foreach (var kek in Chrome.GetSavedPasswords())
+            Console.Title = "Simple Chrome Password Recovery";
+            WriteLineColor(ConsoleColor.Red, "\t Simple Chrome Password Recovery\n");
+
+            foreach (var chrome in Chrome.GetSavedPasswords())
             {
-                Console.WriteLine("SITE: " + kek.URL);
-                Console.WriteLine("USER: " + kek.Username);
-                Console.WriteLine("PASSWORD: " + kek.Password);
+                WriteColor(ConsoleColor.White, "SITE:     ");
+                WriteLineColor(ConsoleColor.White, chrome.URL);
+
+                WriteColor(ConsoleColor.White, "USER:     ");
+                WriteLineColor(ConsoleColor.Blue, chrome.Username);
+
+                WriteColor(ConsoleColor.White, "PASSWORD: ");
+                WriteLineColor(ConsoleColor.Red, chrome.Password);
+
                 Console.WriteLine(Environment.NewLine);
-
             }
-            Console.ReadKey();
 
+            WriteLineColor(ConsoleColor.Red, "\t Todas as senhas encontradas foram recuperadas.");
+            Console.ReadKey();
+        }
+
+        static void WriteColor(ConsoleColor _foregroundColor, String _text)
+        {
+            Console.ForegroundColor = _foregroundColor;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(_text);
+            Console.ResetColor();
+        }
+
+        static void WriteLineColor(ConsoleColor _foregroundColor, String _text)
+        {
+            Console.ForegroundColor = _foregroundColor;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(_text);
+            Console.ResetColor();
         }
     }
 
-   
+
 }
