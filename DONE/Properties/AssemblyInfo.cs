278df0526b3de0d756d942f8969c1981ce135c69
Guilherme Lima <guilherme@amoestudar.onmicrosoft.com>
2018-06-26 00:17:33 -0300

    Atualização.

--- DONE/Properties/AssemblyInfo.cs
+++ DONE/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DONE")]
+[assembly: AssemblyTitle("SimpleChromePwrec")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DONE")]
+[assembly: AssemblyProduct("SimpleChromePwrec")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
